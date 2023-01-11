4cd62f656cae37f67d8b86d42be44ece455796e0
Shinyoung <106143577+syryuauros@users.noreply.github.com>
2023-01-11 14:57:08 +0900

    Update config.el

--- config.el
+++ config.el
@@ -343,20 +343,20 @@
 ;;https://github.com/org-roam/org-roam-ui#package.el--230111-start----------------------------------------
 
 ;;https://github.com/org-roam/org-roam-ui#package.el -start-----------------------------------
-;;(use-package! websocket
-;;    :after org-roam)
+(use-package! websocket
+    :after org-roam)
 
-;;(use-package! org-roam-ui
+(use-package! org-roam-ui
 ;; ;;    :after org-roam ;; or :after org
 ;; ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
 ;; ;;         a hookable mode anymore, you're advised to pick something yourself
 ;; ;;         if you don't care about startup time, use
 ;; ;;  :hook (after-init . org-roam-ui-mode)
-;;    :config
-;;    (setq org-roam-ui-sync-theme t
-;;          org-roam-ui-follow t
-;;          org-roam-ui-update-on-save t
-;;          org-roam-ui-open-on-start t))
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
 ;;https://github.com/org-roam/org-roam-ui#package.el - end-----------------------------------
 
 
