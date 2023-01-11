808c8f8486f1201c5bbc6efb7124d48c310f6336
Shinyoung <106143577+syryuauros@users.noreply.github.com>
2023-01-11 10:00:43 +0900

    Update config.el

--- config.el
+++ config.el
@@ -326,6 +326,24 @@
   :after all-the-icons dired
   :hook (dired-mode . all-the-icons-dired-mode))
 
+;;https://github.com/org-roam/org-roam-ui#package.el -start-----------------------------------
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+;;https://github.com/org-roam/org-roam-ui#package.el - end-----------------------------------
+
+
 (setq which-key-idle-delay 0.1)
 
 (map! :leader
